706fcdd395bbcce985b06594c92aa6812ce1a1c4
rgr19 <onceawaken@outlook.com>
2020-02-03 17:24:54 +0100

    Last commit in branch

--- npm_install_all.sh
+++ npm_install_all.sh
@@ -1,18 +1,50 @@
 #!/usr/bin/env bash
 
+
+npm -g list recursive-install -q
+if [ $? -ne 0 ]; then
+  sudo npm i -g recursive-install -q
+fi
+
+npm-recursive-install -q
+
+
+exit 0
+
+if [ -z "$NPM_INSTALL_FAST" ]; then
+  NPM_INSTALL_FAST=false
+fi
+
 DIRS="$(find $PWD -maxdepth 2 -name package.json -type f)"
 
-CWD=$PWD
+if [ "$NPM_INSTALL_FAST" == 'false' ]; then
 
-FAILURES_FILE=$CWD/npm_install_failed.txt
-SUCCESS_FILE=$CWD/npm_install_success.txt
+  for d in $DIRS; do
+    echo "NPM INSTALL $d"
+    if [ -d "$(dirname $d)" ]; then
+      cd "$(dirname $d)"
+    else
+      echo "no such directory, skipping"
+      continue
+    fi
+    echo "not in success file"
+    npm install --silent
+    npm audit fix --silent
+  done
 
-touch $FAILURES_FILE
-touch $SUCCESS_FILE
+else
 
-FAILURES_ARRAY=()
+  CWD=$PWD
 
-for d in $DIRS; do
+  FAILURES_FILE=$CWD/npm_install_failed.txt
+  SUCCESS_FILE=$CWD/npm_install_success.txt
+
+  touch $FAILURES_FILE
+  touch $SUCCESS_FILE
+
+  FAILURES_ARRAY=()
+
+  for d in $DIRS; do
     echo "NPM INSTALL $d"
     if [ -d "$(dirname $d)" ]; then
       cd "$(dirname $d)"
@@ -31,16 +63,18 @@ for d in $DIRS; do
         FAILURES_ARRAY+=("$d")
       else
         echo "installation successful"
-        echo "$d" >> $SUCCESS_FILE
+        echo "$d" >>$SUCCESS_FILE
       fi
     else
       echo "in success file, skipping"
     fi
     cd $CWD || true
-done
+  done
 
-echo "" > $FAILURES_FILE
+  echo "" >$FAILURES_FILE
 
-for d in "${FAILURES_ARRAY[@]}"; do
-  echo "$d" >> $FAILURES_FILE
-done
+  for d in "${FAILURES_ARRAY[@]}"; do
+    echo "$d" >>$FAILURES_FILE
+  done
+
+fi
