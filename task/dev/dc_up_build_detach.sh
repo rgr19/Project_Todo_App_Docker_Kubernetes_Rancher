bcbdfe20e9c3693834884b68753e8d1422f7e9c9
rgr19 <onceawaken@outlook.com>
2020-02-05 10:59:06 +0100

    Rename .travis.yaml to .travis.yml

--- task/dev/dc_up_build_detach.sh
+++ task/dev/dc_up_build_detach.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # docker-compose [-f <arg>...] [options] [COMMAND] [ARGS...]
-# docker-compose -f ../common/docker-compose.yaml -f docker-compose.override.yaml up --build -d
+# docker-compose -f ../common/docker-compose.yml -f docker-compose.override.yml up --build -d
 docker-compose  up --build -d
